c2d2c67ef4228112003edb784c626e7175f44ff4
localhost4096 <jtgemarino22@src.pshs.edu.ph>
2025-03-18 08:20:07 +0800

    Game (Part 5)

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,81 @@
+var system_array = {}
+
+/*
+    TODO:
+        Create processing function for all variables
+            'NORMAL'           normal processing
+            'CHAMBER_DAMAGE'   
+            'BURNT'             
+            'DESTROYED'        nothing happens, everything is dead
+        Cure to cancer
+        Random events (the game concept right there)
+        fix bug: TEMPLATE_UNIT is being referenced and not copied when passing as an argument
+*/
+
+const TEMPLATE_UNIT = {
+    variables: {
+        on_fire: false,          // simple boolean variables
+        destroyed: false,        // -
+        disconnected: false,     // -
+        chamber_damage: false,   // -
+
+        processor_temperature: 0,// processing temperature (in kelvin)
+        chamber_pressure: 1,     // pressure (in atm)
+
+        coolant_temperature: 25, // temperature (in kelvin)
+        coolant_amount: 0,       // amount (in liters)
+        coolant_in_flow: 0,      // rate of flow (in liters per second)
+        coolant_out_flow: 0,     // rate of flow (in liters per second)
+
+        power_in: 0,             // amount of power (in kilowatts)
+        unit_health: 100,        // general health
+
+        sync_level: 0,           // synchronization level (must be matched with other units)
+        queue: 0,                // amount in queue to process
+        processor_time: 0,       // time to clear an item out of the queue
+
+        _chamber_pressure_rate: 0,// rate of ascent/descent [negative - down / positive - up]
+        _coolant_temp_rate: 0,    // the underscore denotes that it is not accessible by normal means
+        _coolant_amount_rate: 0,  // -
+        _processor_temp_rate: 0,  // -
+
+        _max_coolant_temp: 0,     // maximum amount that is available
+        _max_coolant_in_flow: 0,  // the underscore denotes that it is not accessible by normal means
+        _max_coolant_out_flow: 0, // -
+        _max_coolant_amount: 0,   // -
+        
+        _max_processor_temp: 0,   // -
+        _max_chamber_pressure: 0, // -
+        _max_power_in: 0,         // -
+    },
+
+    NORMAL: state_machine => {
+        console.log("im in a state machine!", state_machine)
+        console.log(state_machine.get_value_table())
+    },
+
+    CHAMBER_DAMAGE: state_machine => {
+        
+    },
+
+    BURNT: state_machine => {
+
+    },
+
+    DESTROYED: state_machine => {
+        
+    },
+}
+
+for (let i = 0; i < 12; i++) {
+    system_array[i] = new StateMachine(TEMPLATE_UNIT, "NORMAL");
+}
+
+system_array[0].set_value("destroyed", true);
+for (let i = 0; i < 12; i++) {
+    system_array[i].change_state("NORMAL");
+}
+
 function trigger(n){
     console.log("clicked on ",n)
 }
--- scripts/libraries/state_machine.js
+++ scripts/libraries/state_machine.js
@@ -6,18 +6,29 @@ class StateMachine {
     #states = {};
     #current_state = null;
 
-    constructor(states=null) {
+    constructor(states=null, default_state=null) {
         if (!states) {
             console.warn("No states have been set, beware! Use set_state_dict() to set states.");
             return false;
         };
 
-        this.set_state_dict(states)
+        this.set_state_dict(states);
+
+        if (default_state) {
+            this.change_state(default_state);
+            return true;
+        };
+        console.warn("No default state used. Manually use set_state() to start the state machine.");
+        return true;
     };
 
     set_state(name, state_function) {
         // sets a new state
         if (this.#states[name]) {
+            return false;
+        };
+
+        if (state_function == null) {
             return false;
         };
         
@@ -37,6 +48,8 @@ class StateMachine {
 
     set_state_dict(states) {
         // overrides the states
+        this.#states["variables"] = states["variables"];
+
         for(let [name, _] of Object.entries(this.#states)) {
             this.remove_state(name);
         };
@@ -59,7 +72,21 @@ class StateMachine {
         };
 
         this.#current_state = name;
-        this.#states[name]();
+        this.#states[name](this);
         return true;
+    }
+
+    set_value(variable, value) {
+
+        this.#states["variables"][variable] = value;
+        return this.#states["variables"][variable];
+    }
+
+    get_value(variable) {
+        return this.#states["variables"][variable];
+    }
+
+    get_value_table() {
+        return this.#states["variables"];
     }
 }
