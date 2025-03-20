feaba0e3614610a8165af00c647b82def1e57e96
localhost4096 <jtgemarino22@src.pshs.edu.ph>
2025-03-20 14:38:32 +0800

    ewrtjk

--- scripts/game.js
+++ scripts/game.js
@@ -2,9 +2,11 @@ var system_array = {};
 var variable_tick_array = {};
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max) // RAHHHh!hh!h!h!h!h!h!h!h!h
+const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
 
 /*
     TODO:
+        * note: finish normal first before continuing to other functions
         Create processing function for all variables
             'NORMAL'           normal processing
             'CHAMBER_DAMAGE'   
@@ -59,38 +61,52 @@ function template_unit_generate(identifier) {
         _coolant_temp_rate: 0,    
         _coolant_amount_rate: 0,  
         _processor_temp_rate: 0,  
+        _unit_health_rate: 0, 
 
         disconnected_signal: new Signal(),
         destroyed_signal: new Signal(),
         on_fire_signal: new Signal(),
+        stop_fire: new Signal(),
     }
 
     unit.NORMAL = (state_machine, tick) => {
-        // ! No power_in and unit_health implementation added. Please do in next revision
+        // !  No unit_health implementation added. Please do in next revision
+        // !  Clamp everything in the next revision
 
         // setting coolant amount rate
         state_machine.set_value("_coolant_amount_rate", 
-            clamp((state_machine.get_value("coolant_in_flow") + state_machine.get_value("coolant_out_flow")), 0, 1)
+            clamp((state_machine.get_value("coolant_in_flow") + state_machine.get_value("coolant_out_flow")).toFixed(2), 0, 1)
         );
 
         // change coolant temperatures based on amount and processor temperatures
         // ? the coldest that the coolant can get is -600 kelvin
         // ? the coldest that the processor can get is -200 kelvin
 
-        // ! UNFINISHED set temperature rates for processor and coolant
+        // ? temporary fix - set temperature rates for processor and coolant
 
         // check if the coolant is stagnant 
-        if (state_machine.get_value("coolant_in_flow") < 1.25 || state_machine.get_value("coolant_out_flow") > -1.25) {
-            // ! UNFINISHED stagnant coolant section
+        if (state_machine.get_value("coolant_in_flow") < 1 || state_machine.get_value("coolant_out_flow") > -1) {
+            // * temporary placeholder
             state_machine.set_value("_coolant_temp_rate", 
-                
+                0.05
             );
         } else {
-            // ! UNFINISHED flowing coolant section 
+            // * temporary placeholder
+            if (state_machine.get_value("coolant_temperature") > -30) {
+                state_machine.set_value("_coolant_temp_rate", 
+                    -0.5
+                );
+                return;
+            };
             state_machine.set_value("_coolant_temp_rate", 
-
+                0
             );
         };
+
+        state_machine.set_value("_processor_temp_rate",
+            state_machine.get_value("coolant_temperature")/1000
+        )
+        
 
         // chamber pressure processing
         // check if coolant amount is over 250 liters.
@@ -98,17 +114,17 @@ function template_unit_generate(identifier) {
         
         if (state_machine.get_value("coolant_amount") > 250) {
             let excess = state_machine.get_value("coolant_amount") - 250
-
             state_machine.set_value("_chamber_pressure_rate",
-                clamp(parseFloat((0.001*(Math.pow(1.01, excess - 10) - 1)).toFixed(2)), 0, 0.03) // * make a more realistic pressure gen
+                parseFloat((0.1*(Math.pow(1.01, excess - 10) - 1)).toFixed(2)) // * make a more realistic pressure gen
             );
+
         } else {
             state_machine.set_value("_chamber_pressure_rate", 0);
         };
 
         // updating coolant amount
         state_machine.set_value("coolant_amount",
-            clamp(state_machine.get_value("coolant_amount") + state_machine.get_value("_coolant_amount_rate"), 0, 1000)
+            parseFloat(state_machine.get_value("coolant_amount") + state_machine.get_value("_coolant_amount_rate"))
         );
 
         // updating processor temperatures
@@ -118,7 +134,7 @@ function template_unit_generate(identifier) {
 
         // updating coolant temperatures
         state_machine.set_value("coolant_temperature",
-            clamp(state_machine.get_value("coolant_temperature") + state_machine.get_value("_coolant_temp_rate"), -600, 300)
+            state_machine.get_value("coolant_temperature") + state_machine.get_value("_coolant_temp_rate")
         );
 
         // updating chamber pressure
@@ -126,16 +142,27 @@ function template_unit_generate(identifier) {
             clamp(state_machine.get_value("chamber_pressure") + state_machine.get_value("_chamber_pressure_rate"), 0, 40)
         );
 
-        console.log(state_machine.get_value("chamber_pressure"), " rate: ", state_machine.get_value("_chamber_pressure_rate"))
-        if (state_machine.get_value("chamber_pressure") >= 40) {
-            console.log("coolant overflow")
+        ////console.log(state_machine.get_value("chamber_pressure"), " rate: ", state_machine.get_value("_chamber_pressure_rate"))
 
+        if (state_machine.get_value("chamber_pressure") >= 40) {
+            // chamber damage event
             state_machine.set_value("chamber_damage", true);
             state_machine.change_state("CHAMBER_DAMAGE");
-        }
+        };
+
+        if (state_machine.get_value("processor_temperature") > 100) {
+            // fire event
+            state_machine.get_value("on_fire", true);
+            state_machine.set_value("chamber_damage", true);
+            state_machine.get_value("on_fire_signal").fire();
+        };
 
         // ? "updates every half a second" section
         if (tick <= 10) {
+            return;
+        };
+
+        if (state_machine.get_value("power_in") < 50) { // power_in implementation
             return;
         };
 
@@ -164,11 +191,23 @@ function template_unit_generate(identifier) {
 // generate new statemachines for the units
 for (let i = 0; i < 12; i++) {
     system_array[i] = new StateMachine(template_unit_generate(i+1), "NORMAL");
+
+    system_array[i].get_value("stop_fire").connect("fire_extinguished", event => {
+        if (!system_array[i].get_value("on_fire")) {
+            return;
+        };
+
+        console.log("Fire stopped!")
+        system_array[i].set_value("on_fire", false);
+        system_array[i].change_state("BURNT");
+    });
 }
 
 function trigger(n){
     console.log("clicked on ",n)
 }
+
+
 
 // unit interval
 setInterval(event => {
@@ -179,5 +218,19 @@ setInterval(event => {
 
 // observer interval
 setInterval(event => {
-    console.log(system_array[0].get_value_table())
+    // attach them to the panels
+    for (let i = 1; i < 13; i++) {
+        switch(system_array[i-1].get_state()) {
+            case "NORMAL":
+                document.getElementById("unit-"+i).style.animation = "none";
+            case "CHAMBER_DAMAGE":
+                document.getElementById("unit-"+i).style.animation = "flash-two 1s linear infinite";
+            case "BURNT":
+                document.getElementById("unit-"+i).style.animation = "flash-three 1s linear infinite";
+            case "DESTROYED":
+                document.getElementById("unit-"+i).style.animation = "dead 1s linear infinite";
+        }
+        console.log("updated! ", "unit-"+i)
+    }
 }, 1000)
+
