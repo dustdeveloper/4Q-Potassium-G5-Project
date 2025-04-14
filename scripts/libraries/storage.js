1cac55afb604c0a92f8a5f620d2e73cc4ff63010
dust developer <jtgemarino22@src.pshs.edu.ph>
2025-04-14 09:50:11 +0800

    edits to the storage library

--- scripts/libraries/storage.js
+++ scripts/libraries/storage.js
@@ -1,42 +1,34 @@
 // storage implementation
 
-class StorageInterface {
+class StorageClass {
     constructor() {};
 
-    _is_outdated() {
+    static _is_outdated() {
         return typeof(Storage) == "undefined";
     };
 
     static get_local_item(item_name) {
-        if (_is_outdated()) {
-            return null;
-        };
+        if (StorageClass._is_outdated()) { return null; };
 
         return localStorage.getItem(item_name);
     };
 
     static set_local_item(item_name, value) {
-        if (_is_outdated()) {
-            return null;
-        };
+        if (StorageClass._is_outdated()) { return null; };
 
         localStorage.setItem(item_name, value);
         return true;
     };
 
     static remove_local_item(item_name) {
-        if (_is_outdated()) {
-            return null;
-        };
+        if (StorageClass._is_outdated()) { return null; };
 
         localStorage.removeItem(item_name);
         return true;
     };
 
     static clear() {
-        if (_is_outdated()) {
-            return null;
-        };
+        if (StorageClass._is_outdated()) { return null; };
 
         localStorage.clear();
         return true;
