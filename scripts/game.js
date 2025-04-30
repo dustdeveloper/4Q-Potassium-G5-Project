b2fc0c83c37cebb3983fa40a5ac1514fe6090c84
localhost4096 <jtgemarino22@src.pshs.edu.ph>
2025-04-30 14:07:20 +0800

    major shit

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 var system_array = {};
 var variable_tick_array = {};
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max) // RAHHHh!hh!h!h!h!h!h!h!h!h
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max) // Range Limit
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
 
 /*
@@ -70,107 +70,107 @@ function template_unit_generate(identifier) {
     }
 
     unit.NORMAL = (state_machine, tick) => {
-        // !  No unit_health implementation added. Please do in next revision
-        // !  Clamp everything in the next revision
+        // // !  No unit_health implementation added. Please do in next revision
+        // // !  Clamp everything in the next revision
 
-        // setting coolant amount rate
-        state_machine.set_value("_coolant_amount_rate", 
-            clamp((state_machine.get_value("coolant_in_flow") + state_machine.get_value("coolant_out_flow")).toFixed(2), 0, 1)
-        );
+        // // setting coolant amount rate
+        // state_machine.set_value("_coolant_amount_rate", 
+        //     clamp((state_machine.get_value("coolant_in_flow") + state_machine.get_value("coolant_out_flow")).toFixed(2), -1, 1)
+        // );
 
-        // change coolant temperatures based on amount and processor temperatures
-        // ? the coldest that the coolant can get is -600 kelvin
-        // ? the coldest that the processor can get is -200 kelvin
+        // // change coolant temperatures based on amount and processor temperatures
+        // // ? the coldest that the coolant can get is -600 kelvin
+        // // ? the coldest that the processor can get is -200 kelvin
 
-        // ? temporary fix - set temperature rates for processor and coolant
+        // // ? temporary fix - set temperature rates for processor and coolant
 
-        // check if the coolant is stagnant 
-        if (state_machine.get_value("coolant_in_flow") < 1 || state_machine.get_value("coolant_out_flow") > -1) {
-            // * temporary placeholder
-            state_machine.set_value("_coolant_temp_rate", 
-                0.05
-            );
-        } else {
-            // * temporary placeholder
-            if (state_machine.get_value("coolant_temperature") > -30) {
-                state_machine.set_value("_coolant_temp_rate", 
-                    -0.5
-                );
-                return;
-            };
-            state_machine.set_value("_coolant_temp_rate", 
-                0
-            );
-        };
+        // // check if the coolant is stagnant 
+        // if (state_machine.get_value("coolant_in_flow") < 1 || state_machine.get_value("coolant_out_flow") > -1) {
+        //     // * temporary placeholder
+        //     state_machine.set_value("_coolant_temp_rate", 
+        //         0.05
+        //     );
+        // } else {
+        //     // * temporary placeholder
+        //     if (state_machine.get_value("coolant_temperature") > -30) {
+        //         state_machine.set_value("_coolant_temp_rate", 
+        //             -0.5
+        //         );
+        //         return;
+        //     };
+        //     state_machine.set_value("_coolant_temp_rate", 
+        //         0
+        //     );
+        // };
 
-        state_machine.set_value("_processor_temp_rate",
-            state_machine.get_value("coolant_temperature")/1000
-        )
+        // state_machine.set_value("_processor_temp_rate",
+        //     state_machine.get_value("coolant_temperature")/1000
+        // )
         
 
-        // chamber pressure processing
-        // check if coolant amount is over 250 liters.
-        // if over, count the excess and plug it into a exponential func and set it on _chamber_pressure_rate
+        // // chamber pressure processing
+        // // check if coolant amount is over 250 liters.
+        // // if over, count the excess and plug it into a exponential func and set it on _chamber_pressure_rate
         
-        if (state_machine.get_value("coolant_amount") > 250) {
-            let excess = state_machine.get_value("coolant_amount") - 250
-            state_machine.set_value("_chamber_pressure_rate",
-                parseFloat((0.1*(Math.pow(1.01, excess - 10) - 1)).toFixed(2)) // * make a more realistic pressure gen
-            );
+        // if (state_machine.get_value("coolant_amount") > 250) {
+        //     let excess = state_machine.get_value("coolant_amount") - 250
+        //     state_machine.set_value("_chamber_pressure_rate",
+        //         parseFloat((0.1*(Math.pow(1.01, excess - 10) - 1)).toFixed(2)) // * make a more realistic pressure gen
+        //     );
 
-        } else {
-            state_machine.set_value("_chamber_pressure_rate", 0);
-        };
+        // } else {
+        //     state_machine.set_value("_chamber_pressure_rate", 0);
+        // };
 
-        // updating coolant amount
-        state_machine.set_value("coolant_amount",
-            parseFloat(state_machine.get_value("coolant_amount") + state_machine.get_value("_coolant_amount_rate"))
-        );
+        // // updating coolant amount
+        // state_machine.set_value("coolant_amount",
+        //     parseFloat(state_machine.get_value("coolant_amount") + state_machine.get_value("_coolant_amount_rate"))
+        // );
 
-        // updating processor temperatures
-        state_machine.set_value("processor_temperature",
-            clamp(state_machine.get_value("processor_temperature") + state_machine.get_value("_processor_temp_rate"), -200, 300)
-        );
+        // // updating processor temperatures
+        // state_machine.set_value("processor_temperature",
+        //     clamp(state_machine.get_value("processor_temperature") + state_machine.get_value("_processor_temp_rate"), -200, 300)
+        // );
 
-        // updating coolant temperatures
-        state_machine.set_value("coolant_temperature",
-            state_machine.get_value("coolant_temperature") + state_machine.get_value("_coolant_temp_rate")
-        );
+        // // updating coolant temperatures
+        // state_machine.set_value("coolant_temperature",
+        //     state_machine.get_value("coolant_temperature") + state_machine.get_value("_coolant_temp_rate")
+        // );
 
-        // updating chamber pressure
-        state_machine.set_value("chamber_pressure",
-            clamp(state_machine.get_value("chamber_pressure") + state_machine.get_value("_chamber_pressure_rate"), 0, 40)
-        );
+        // // updating chamber pressure
+        // state_machine.set_value("chamber_pressure",
+        //     clamp(state_machine.get_value("chamber_pressure") + state_machine.get_value("_chamber_pressure_rate"), 0, 40)
+        // );
 
-        ////console.log(state_machine.get_value("chamber_pressure"), " rate: ", state_machine.get_value("_chamber_pressure_rate"))
+        // ////console.log(state_machine.get_value("chamber_pressure"), " rate: ", state_machine.get_value("_chamber_pressure_rate"))
 
-        if (state_machine.get_value("chamber_pressure") >= 40) {
-            // chamber damage event
-            state_machine.set_value("chamber_damage", true);
-            state_machine.change_state("CHAMBER_DAMAGE");
-        };
+        // if (state_machine.get_value("chamber_pressure") >= 40) {
+        //     // chamber damage event
+        //     state_machine.set_value("chamber_damage", true);
+        //     state_machine.change_state("CHAMBER_DAMAGE");
+        // };
 
-        if (state_machine.get_value("processor_temperature") > 100) {
-            // fire event
-            state_machine.get_value("on_fire", true);
-            state_machine.set_value("chamber_damage", true);
-            state_machine.get_value("on_fire_signal").fire();
-        };
+        // if (state_machine.get_value("processor_temperature") > 100) {
+        //     // fire event
+        //     state_machine.get_value("on_fire", true);
+        //     state_machine.set_value("chamber_damage", true);
+        //     state_machine.get_value("on_fire_signal").fire();
+        // };
 
-        // ? "updates every half a second" section
-        if (tick <= 10) {
-            return;
-        };
+        // // ? "updates every half a second" section
+        // if (tick <= 10) {
+        //     return;
+        // };
 
-        if (state_machine.get_value("power_in") < 50) { // power_in implementation
-            return;
-        };
+        // if (state_machine.get_value("power_in") < 50) { // power_in implementation
+        //     return;
+        // };
 
-        let item = state_machine.get_value("queue")[0];
-        if (typeof(item) == "function") {
-            item(state_machine);
-            state_machine.set_value("sync_level", state_machine.get_value("sync_level") + 1)
-        };
+        // let item = state_machine.get_value("queue")[0];
+        // if (typeof(item) == "function") {
+        //     item(state_machine);
+        //     state_machine.set_value("sync_level", state_machine.get_value("sync_level") + 1)
+        // };
     };
 
     unit.CHAMBER_DAMAGE = (state_machine, tick) => { // relies on chamber_damage variable
---
+++ scripts/game/intro.js
@@ -0,0 +1,251 @@
+var intro_timeline = gsap.timeline();
+
+var complete_signal = new Signal();
+var intro_complete = false;
+
+function add_to(text, side) {
+    let new_paragraph = document.createElement("p");
+    new_paragraph.textContent = text;
+
+    let target = side === "left" 
+        ? document.getElementById("left-intro-indicator") 
+        : document.getElementById("right-intro-indicator");
+    target.appendChild(new_paragraph);
+}
+
+function clear() {
+    let left_indicator = document.getElementById("left-intro-indicator");
+    let right_indicator = document.getElementById("right-intro-indicator");
+
+    while (left_indicator.firstChild) {
+        left_indicator.removeChild(left_indicator.firstChild);
+    }
+
+    while (right_indicator.firstChild) {
+        right_indicator.removeChild(right_indicator.firstChild);
+    }
+}
+
+function new_modal() {
+    let original = document.querySelector("#percent50-background");
+    let clone = original.cloneNode(true);
+
+    gsap.to(clone, {zIndex: 100, duration: 0});
+
+    clone.classList.add("clone");
+
+    let modal = clone.children[0];
+    modal.id = "clone";
+
+    while (modal.firstChild) {
+        modal.removeChild(modal.firstChild);
+    }
+    console.log(clone);
+    document.body.appendChild(clone);
+
+    return clone
+}
+
+function append_new_element(type, parent, id="", typeclass="", style={}) {
+    let item = document.createElement(type);
+
+    if (typeclass !== "") {
+        item.classList.add(typeclass);
+    }
+    if (id !== "") {
+        item.setAttribute("id", id);
+    }
+    for (let key in style) {
+        item.style[key] = style[key];
+    }
+
+    parent.appendChild(item);
+
+    return item;
+}
+
+function intro_modal() {
+    complete_signal.connect("listener", () => {
+        complete_signal.disconnect("listener");
+        add_to("Finding session data...", "left");
+        add_to("[ OK ]", "right");
+
+        get_session_data()
+        document.querySelectorAll('.clone').forEach(clone => clone.remove());
+    });
+
+    let modal = new_modal().children[0];
+    let input = new Input();
+
+    gsap.to(modal, {width: "fit-content", height: "fit-content",left: "50%", top: "70%", fontFamily: "Inconsolata", textAlign: "center", duration: 0});
+    
+    let title = append_new_element("h3", modal, "","", {margin: 0});
+    let subtitle = append_new_element("h4", modal, "","", {margin: "-2%", fontWeight: "normal"});
+    title.innerHTML = "KEYBOARD MODULE CHECK";
+    subtitle.innerHTML = "hold down the keys for 5 seconds.";
+
+    let container = append_new_element("div", modal, "","", {margin: 0, display: "flex", justifyContent: "space-around", alignItems: "center"});
+
+    let keys = "qwop";
+    let key_array = {};
+    let countdownInterval = null;
+
+    // Create divs for each key
+    for (let key of keys) {
+        let div = append_new_element("div", container, key, "key-indicator", {fontSize: "2em", fontWeight: "bold"});
+        div.textContent = key.toUpperCase();
+
+        key_array[key] = false;
+
+        input.keyDown.connect("keylistener" + key, key_down => {
+            if (key_down[0].toLowerCase() === key) {
+                div.style.color = "green";
+                key_array[key] = true;
+                checkKeysAndStartCountdown();
+            }
+        });
+
+        input.keyUp.connect("keylistener" + key, key_up => {
+            if (key_up[0].toLowerCase() === key) {
+                div.style.color = "white";
+                key_array[key] = false;
+                stopCountdown();
+            }
+        });
+    }
+
+    function checkKeysAndStartCountdown() {
+        // Check if all keys are true
+        if (Object.values(key_array).every(value => value)) {
+            if (!countdownInterval) {
+                startCountdown();
+            }
+        }
+    }
+
+    function startCountdown() {
+        let countdown = 5; // Countdown starts at 5 seconds
+        console.log("Countdown started!");
+
+        countdownInterval = setInterval(() => {
+            subtitle.innerHTML = "Hold for : " + countdown;
+
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
+                countdownInterval = null;
+
+                complete_signal.fire();
+                input.destroy();
+            }
+            countdown--;
+        }, 1000);
+    }
+
+    function stopCountdown() {
+        if (countdownInterval) {
+            clearInterval(countdownInterval);
+            countdownInterval = null;
+            console.log("Countdown stopped!");
+            subtitle.innerHTML = "hold down the keys for 5 seconds.";
+        }
+    }
+
+    return true;
+}
+
+function get_session_data() {
+    complete_signal.connect("listener", () => {
+        complete_signal.disconnect("listener");
+        add_to("Moving session data...", "left");
+        add_to("[ OK ]", "right");
+
+        // next function
+    });
+
+    let data = Session.get("parameters", true);
+    if (!data) {
+        add_to("- No session data active! Reusing data...", "left");
+        add_to("-", "right");
+
+
+    }
+
+    complete_signal.fire();
+}
+
+
+
+function interactive() {
+    // open modal prompt for keyboard
+    add_to("Checking module keyboard...", "left");
+
+    intro_modal()
+
+    // // move data to localstorage
+    // add_to("Moving session data to LOCALSTORAGE..", "left");
+
+    // add_to("[ OK ]", "right");
+}
+
+intro_timeline
+    // initial intro left
+    .call(add_to, ["ENCODER INITIATIVE (EI) 2001-2015", "left"], 0)
+    .call(add_to, ["Property of EI. Do not replicate nor reproduce this software/hardware.", "left"], 0)
+    .call(add_to, ["Any attempts to do such will be faced with repercussions.", "left"], 0)
+    .call(add_to, ["SANITY CHECKING (1 of 5)", "left"], 1)
+    .call(add_to, ["SANITY CHECKING (2 of 5)", "left"], 1.1)
+    .call(add_to, ["SANITY CHECKING (3 of 5)", "left"], 1.1)
+    .call(add_to, ["SANITY CHECKING (4 of 5)", "left"], 1.1)
+    .call(add_to, ["SANITY CHECKING (5 of 5)", "left"], 1.3)
+    .call(add_to, ["AUTHENTICATING MODULES", "left"], 1.4)
+    // initial intro right
+    .call(add_to, ["-", "right"], 0)
+    .call(add_to, ["-", "right"], 0)
+    .call(add_to, ["-", "right"], 0)
+    .call(add_to, ["[ OK ]", "right"], 1)
+    .call(add_to, ["[ OK ]", "right"], 1.1)
+    .call(add_to, ["[ OK ]", "right"], 1.1)
+    .call(add_to, ["[ OK ]", "right"], 1.1)
+    .call(add_to, ["[ OK ]", "right"], 1.4)
+    .call(add_to, ["[ OK ]", "right"], 1.7)
+
+    .call(clear, null, 2)
+    // initial intro 2 left
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2)
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2.05)
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2.05)
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2.2)
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2.2)
+    .call(add_to, ["WARNING! MODIFIED REGISTER DETECTED", "left"], 2.3)
+    .call(add_to, ["CRITICAL! System Optimizations have failed.", "left"], 3)
+    .call(add_to, ["- open log to view error", "left"], 3)
+    .call(clear, null, 2)
+    .call(add_to, ["0x73F0DA", "right"], 2)
+    .call(add_to, ["0x03905C", "right"], 2.05)
+    .call(add_to, ["0xAD8F3D", "right"], 2.05)
+    .call(add_to, ["0x0AF85D", "right"], 2.2)
+    .call(add_to, ["0xB1FF7F", "right"], 2.2)
+    .call(add_to, ["0xAD1B19", "right"], 2.2)
+
+    .call(clear, null, 3.2)
+    // initial intro 3 left
+    .call(add_to, ["Loading Unit monitor1", "left"], 3.2)
+    .call(add_to, ["Loading Unit submonitor2", "left"], 3.2)
+    .call(add_to, ["Loading Unit submonitor3", "left"], 3.2)
+    .call(add_to, ["Loading Unit interfacemanager", "left"], 3.3)
+    .call(add_to, ["P2P integration...", "left"], 4)
+    .call(add_to, ["Running checks...", "left"], 4)
+    .to("#intro", {opacity: 0, duration: 0}, 4.5)
+    .to("#intro", {opacity: 1, width: "100%", height: "80%", duration: 0}, 4.6)
+    .call(add_to, ["PREREQUISITES COMPLETE! Loading...", "left"], 5)
+    // initial intro 3 right
+    .call(add_to, ["[ OK ]", "right"], 3.2)
+    .call(add_to, ["[ OK ]", "right"], 3.2)
+    .call(add_to, ["[ OK ]", "right"], 3.2)
+    .call(add_to, ["[ OK ]", "right"], 3.7)
+    .call(add_to, ["[ OK ]", "right"],  4)
+    .call(add_to, ["[ OK ]", "right"],  5)
+
+    // interactive intro
+    .call(clear, null, 5.5)
+    .call(interactive, null, 5.5) // call interactive function
